30d4ca474f452dd0cea0d4b630387a6f3653fbb9
peitao <peitao@wanda.cn>
2017-08-20 21:02:52 +0800

    vue-music练习项目提交

---
+++ src/components/m-header/m-header.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="m-header">
+    <div class="icon"></div>
+    <h1 class="text">Crazy Music</h1>
+  </div>
+</template>
+
+<script>
+  export default {}
+</script>
+
+<style lang="scss" rel="stylesheet/scss">
+  @import "~common/scss/variables";
+  @import "~common/scss/mixins";
+  .m-header {
+    height: 44px;
+    text-align: center;
+    color: $color-theme;
+    font-size: 0;
+
+    .icon {
+      display: inline-block;
+      width: 30px;
+      height: 32px;
+      margin-right: 10px;
+      vertical-align: top;
+      @include bg-image("logo");
+      background-size: 30px 32px;
+    }
+
+    .text {
+      display: inline-block;
+      line-height: 44px;
+      font-size: $font-size-large;
+      vertical-align: top;
+    }
+  }
+</style>
---
+++ src/main.js
@@ -0,0 +1,18 @@
+import 'babel-polyfill'
+import fastclick from 'fastclick'
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+
+import 'common/scss/index.scss'
+
+fastclick.attach(document.body)
+
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  render: h => h(App)
+})
