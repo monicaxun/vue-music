9664b6bf95574b25bc1bbc279cf708ce18df6c59
monicaxun <fairy_1512@126.com>
2017-08-22 10:58:26 +0800

    1. Add tab navigator; 2. Modify home base style

---
+++ src/components/tab/tab.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+      <ul>
+        <router-link tag="li" class="tab-item" to="/recommend">推荐</router-link>
+        <router-link tag="li" class="tab-item" to="/recommend">歌手</router-link>
+        <router-link tag="li" class="tab-item" to="/recommend">排行</router-link>
+        <router-link tag="li" class="tab-item" to="/recommend">搜索</router-link>
+      </ul>
+    </div>
+</template>
+
+<script>
+  export default {}
+</script>
+
+<style scoped rel="stylesheet/scss">
+  ul {
+    .tab-item {
+      display: inline-block;
+    }
+  }
+
+</style>
